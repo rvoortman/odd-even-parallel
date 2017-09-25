b5c9b0ce924f886ec29292c74fe0e62473be6f3d
Robbin Voortman <robbinvoortman@gmail.com>
2017-09-25 12:54:26 +0200

    Added Base for OddEvenTranspositionSort algorithm

---
+++ src/com/utwente/sortingalgorithms/OddEvenTranspositionSort.java
@@ -0,0 +1,11 @@
+package com.utwente.sortingalgorithms;
+
+import com.utwente.SortInterface;
+
+public class OddEvenTranspositionSort implements SortInterface{
+
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+}
