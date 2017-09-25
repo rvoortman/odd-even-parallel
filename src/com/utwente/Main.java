b4e636cd4e91a0c1188993cc35eba67075ab2a09
Robbin Voortman <robbinvoortman@gmail.com>
2017-09-25 12:53:26 +0200

    Added testarray

--- src/com/utwente/Main.java
+++ src/com/utwente/Main.java
@@ -3,6 +3,6 @@ package com.utwente;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] testArray = {5,4,3,6,8,7,9,1,2};
     }
 }
