6482f1a3a55a6e4ad53a9e682f697e8d5bfe3b38
Dirk-Jan Wassink <dirk.jan.wassink@gmail.com>
2017-09-25 13:03:57 +0200

    Added a pretty print for int[]

--- src/com/utwente/Main.java
+++ src/com/utwente/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         int[] testArray = {5,4,3,6,8,7,9,1,2};
+        System.out.println(prettyPrintArray(testArray));
+
+    }
+
+    public static String prettyPrintArray(int[] array) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("int[").append(array.length).append("] = {");
+        for (int anArray : array) {
+            sb.append(anArray).append(", ");
+        }
+        sb.setLength(sb.length() - 2);
+        sb.append('}');
+
+        return sb.toString();
     }
 }
